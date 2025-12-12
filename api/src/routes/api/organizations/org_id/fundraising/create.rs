1accaf0bf356d575958ded42afffccde58ee34c6
Piernik <piernik@piernik.rocks>
2025-12-12 16:12:06 +0100

    improved code clarity

---
+++ api/src/routes/api/organizations/org_id/fundraising/create.rs
@@ -0,0 +1,73 @@
+use axum::{Extension, Json, extract::State, middleware};
+use axum_valid::Valid;
+use sea_orm::{ActiveValue::Set, EntityTrait, ModelTrait};
+use utoipa_axum::{router::OpenApiRouter, routes};
+
+use crate::{
+    axum_error::AxumResult,
+    middlewares::{require_auth::UnauthorizedError, require_org_permissions::requre_org_admin},
+    models::{
+        fundraising::{self, MutableFundraising},
+        organization, user,
+    },
+    routes::api::CreateSuccess,
+    state::AppState,
+};
+
+pub fn routes() -> OpenApiRouter<AppState> {
+    OpenApiRouter::new()
+        .routes(routes!(create_fundraising))
+        .layer(middleware::from_fn(requre_org_admin))
+}
+
+/// Create a new fundraising
+#[utoipa::path(
+    method(post),
+    path = "/",
+    request_body = MutableFundraising,
+    responses(
+        (status = OK, description = "Success", body = CreateSuccess),
+        (status = UNAUTHORIZED, description = "Unauthorized", body = UnauthorizedError, content_type = "application/json")
+    ),
+    tag = "Fundraisings"
+)]
+#[axum::debug_handler]
+pub async fn create_fundraising(
+    State(state): State<AppState>,
+    Extension(org_data): Extension<organization::Model>,
+    Valid(body): Valid<Json<MutableFundraising>>,
+) -> AxumResult<Json<CreateSuccess>> {
+    let users = org_data
+        .find_related(user::Entity)
+        .all(&state.sea_orm)
+        .await?;
+
+    if body
+        .payers
+        .iter()
+        .any(|p| !users.iter().any(|u| u.id == p.user_id))
+    {
+        return Err(crate::axum_error::AxumError::forbidden(
+            color_eyre::eyre::eyre!("All payers must be members of the organization"),
+        ));
+    };
+
+    let fundraising_model = fundraising::ActiveModel {
+        description: Set(body.description.clone()),
+        name: Set(body.name.clone()),
+        total_amount: Set(body.total_amount),
+        end_date: Set(body.end_date.map(|d| d.into())),
+        start_date: Set(body.start_date.map(|d| d.into())),
+        organization_id: Set(org_data.id),
+        ..Default::default()
+    };
+
+    let fundraising = fundraising::Entity::insert(fundraising_model)
+        .exec_with_returning(&state.sea_orm)
+        .await?;
+
+    Ok(Json(CreateSuccess {
+        success: true,
+        id: fundraising.id,
+    }))
+}
--- api/src/routes/api/organizations/org_id/fundraising/mod.rs
+++ api/src/routes/api/organizations/org_id/fundraising/mod.rs
@@ -1,79 +1,25 @@
-use axum::{Extension, Json, extract::State, middleware};
-use axum_valid::Valid;
-use sea_orm::{ActiveValue::Set, ColumnTrait, EntityTrait, ModelTrait, QueryFilter};
+mod create;
+
+use axum::{Extension, Json};
+use sea_orm::{ColumnTrait, EntityTrait, ModelTrait, QueryFilter};
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_axum::routes;
 
 use crate::{
     axum_error::AxumResult,
-    middlewares::{require_auth::UnauthorizedError, require_org_permissions::requre_org_admin},
+    middlewares::require_auth::UnauthorizedError,
     models::{
-        fundraising::{self, FundraisingRes, MutableFundraising, Payer},
+        fundraising::{self, FundraisingRes, Payer},
         organization, payers, user,
     },
-    routes::api::CreateSuccess,
     state::AppState,
 };
 
 pub fn routes() -> OpenApiRouter<AppState> {
-    let admin = OpenApiRouter::new()
-        .routes(routes!(create_fundraising))
-        .layer(middleware::from_fn(requre_org_admin));
-    let user = OpenApiRouter::new().routes(routes!(get_fundraisings));
+    let create = create::routes();
+    let router = OpenApiRouter::new().routes(routes!(get_fundraisings));
 
-    admin.merge(user)
-}
-
-/// Create a new fundraising
-#[utoipa::path(
-    method(post),
-    path = "/",
-    request_body = MutableFundraising,
-    responses(
-        (status = OK, description = "Success", body = CreateSuccess),
-        (status = UNAUTHORIZED, description = "Unauthorized", body = UnauthorizedError, content_type = "application/json")
-    ),
-    tag = "Fundraising"
-)]
-#[axum::debug_handler]
-async fn create_fundraising(
-    State(state): State<AppState>,
-    Extension(org_data): Extension<organization::Model>,
-    Valid(body): Valid<Json<MutableFundraising>>,
-) -> AxumResult<Json<CreateSuccess>> {
-    let users = org_data
-        .find_related(user::Entity)
-        .all(&state.sea_orm)
-        .await?;
-
-    if body
-        .payers
-        .iter()
-        .any(|p| !users.iter().any(|u| u.id == p.user_id))
-    {
-        return Err(crate::axum_error::AxumError::forbidden(
-            color_eyre::eyre::eyre!("All payers must be members of the organization"),
-        ));
-    };
-
-    let fundraising_model = fundraising::ActiveModel {
-        description: Set(body.description.clone()),
-        name: Set(body.name.clone()),
-        total_amount: Set(body.total_amount),
-        end_date: Set(body.end_date.map(|d| d.into())),
-        start_date: Set(body.start_date.map(|d| d.into())),
-        organization_id: Set(org_data.id),
-        ..Default::default()
-    };
-
-    let fundraising = fundraising::Entity::insert(fundraising_model)
-        .exec_with_returning(&state.sea_orm)
-        .await?;
-
-    Ok(Json(CreateSuccess {
-        success: true,
-        id: fundraising.id,
-    }))
+    create.merge(router)
 }
 
 /// Get all fundraisings for an organization
@@ -84,7 +30,7 @@ async fn create_fundraising(
         (status = OK, description = "Success", body = Vec<FundraisingRes>, content_type = "application/json"),
         (status = UNAUTHORIZED, description = "Unauthorized", body = UnauthorizedError, content_type = "application/json")
     ),
-    tag = "Fundraising"
+    tag = "Fundraisings"
 )]
 async fn get_fundraisings(
     Extension(state): Extension<AppState>,
